d11346ff1b9cbfd549607cf917c54c262780fd99
Akash Tripathi <akashtripathi.2801@gmail.com>
2021-12-19 00:33:40 +0530

    modified:   App/screens/Feed.jsx

--- App/screens/Feed.jsx
+++ App/screens/Feed.jsx
@@ -6,7 +6,9 @@ import {
     StyleSheet,
     Image,
     View,
-    Modal
+    Modal,
+    Pressable,
+    Dimensions, KeyboardAvoidingView 
 } from 'react-native';
 import { Button, Overlay } from 'react-native-elements';
 import PropTypes from 'prop-types';
@@ -15,7 +17,7 @@ import { fetchImages } from '../util/api';
 import CardList from '../components/CardList';
 
 import colors from '../constants/colors';
-import { TouchableOpacity } from 'react-native-gesture-handler';
+import { TextInput, TouchableOpacity } from 'react-native-gesture-handler';
 import { ScrollView } from 'react-native';
 
 export default class Feed extends Component {
@@ -93,18 +95,49 @@ export default class Feed extends Component {
                     onPressComments={onPressComments}
                 />
                 <Overlay  isVisible={this.state.showPostOverlay} onBackdropPress = {this.closeOverLay}  
-                    overlayStyle={styles.root}
+                    overlayStyle={styles.root} 
                 >
-                    <View>
-
+                    <View style={styles.postBoxContainer}>
+                        <TextInput style={styles.headingContainer}
+                         placeholder="Heading" maxLength={100} 
+                         multiline={true}
+                         
+                         ></TextInput>
+                        <TextInput style={styles.articleContainer}  placeholder="Article" maxLength={800}  multiline={true}></TextInput>
                     </View>
-                    <ScrollView>
 
-                    </ScrollView>
-                    <Button title="Publish" containerStyle={{ height: '10%'}}
-                        buttonStyle={{ width: "20%",  borderRadius: 10,}}  onPress={this.saveToData}/>
+                    <View style={styles.buttonContainer}>
+                        <View style={styles.buttonFlex}>
+                            <Pressable style={styles.loginbutton} 
+                                onPress={this.saveToData}
+                            >
+                            <Text style={styles.textInput}>Publish</Text>
+                            </Pressable>
+                        </View>
+
+                        <View style={styles.buttonFlex}>
+                            <Pressable style={styles.loginbutton}
+                                onPress={this.saveToData}
+                            >
+                            <Text style={styles.textInput}>Draft</Text>
+                            </Pressable>            
+                        </View>
+
+                        <View style={styles.buttonFlex}>
+                            <Pressable style={styles.loginbutton}
+                                onPress={this.saveToData}
+                            >
+                            <Text style={styles.textInput}>Cover Image</Text>
+                            </Pressable>
+                        </View>
+                    </View>
+
+
+    
+
+
                 </Overlay>
-                <Button title="Post" containerStyle={{ height: '12%', margin: 5}}
+                <Button  title="Post" containerStyle={{ height: '12%', margin: 5}}
                         buttonStyle={{ width: "100%",  borderRadius: 35,}}  onPress={this.openOverLay}/>
             </SafeAreaView>
         );
@@ -118,10 +151,60 @@ const styles  = StyleSheet.create(
             height:'100%',
             width:'100%',
             borderRadius:10,
-            justifyContent: 'center',
+            justifyContent: 'flex-start',
             alignItems: 'center',
             backgroundColor: 'yellow',
-          }
+          },
+        postBoxContainer:{
+            flex: 3,
+            height:'100%',
+            width:'100%',
+            borderRadius:10,
+            justifyContent: 'flex-start',
+            alignItems: 'flex-start',
 
+        },
+        articleContainer:{
+            flex:1,
+            height: '120%',
+            width: '100%',
+            fontSize:20,
+            textAlignVertical:"top",
+
+            borderRadius:10
+        },
+        headingContainer:{
+            flex:.2,
+            fontSize:25,
+            borderBottomWidth:1,
+            textAlignVertical:"top",
+            height: '10%',
+            width: '100%',
+
+        },
+        buttonContainer:{
+            flex:.5,
+            flexDirection:"row",
+            height: '100%',
+            width: '100%',
+        },
+        loginbutton:{
+            height:'60%',
+            width:'80%',
+            margin:10,
+            backgroundColor: "dodgerblue",
+            borderRadius:10,
+            fontWeight:"bold",
+            fontSize:18,
+        },
+        textInput:{
+            color:colors.white,
+            padding:10,
+            textAlign:'center',
+            fontSize:18
+        },
+        buttonFlex:{
+            width:Dimensions.get('window').width /3
+        }
     }
 );
